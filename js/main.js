6cb74c37b2fdc70b725cac02fb9892a0fa1c8dc4
diana <dskripka005@gmail.com>
2023-09-21 22:14:47 +0300

    fixfour

--- js/main.js
+++ js/main.js
@@ -30,14 +30,47 @@ console.log(countProps(all));
 
 //3
 function findBestEmployee(employees) {
-    
-}
-
+    let maxxTask = 0;
+    let bestEmployee = '';
+  
+    for (const employee in employees) {
+      const tasksCompleted = employees[employee];
+      
+      if (tasksCompleted > maxxTask) {
+        maxxTask = tasksCompleted;
+        bestEmployee = employee;
+      }
+    }
+  
+    return bestEmployee;
+  }
+const employees = {
+    John: 48,
+    Jane: 50,
+    Bob: 29,
+  };
+  console.log(findBestEmployee(employees));
 //4
 function countTotalSalary(employees) {
-    
+    let finalSalary = 0;
+
+    for (const employee in employees) {
+      if (employees.hasOwnProperty(employee)) {
+        finalSalarySalary += employees[employee];
+      }
+    }
+
+    return finalSalarySalary;
 }
 
+const salary = {
+    he: 10000,
+    she: 2900,
+    jon: 6000,
+    sara: 8000
+};
+
+console.log(countTotalSalary(salary));
 
 //5
 function getAllPropValues(arr, prop) {
